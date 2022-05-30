e84a29ca658b3b0f4a5fe8a5c65c1e00a85d272f
Alexey Plenkin <aveplen@yandex.ru>
2022-05-31 02:04:55 +0300

    modified client to load real file

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io/ioutil"
 
 	"github.com/aveplen-bach/s3/protos/s3file"
 	"google.golang.org/grpc"
@@ -16,9 +17,15 @@ func main() {
 
 	client := s3file.NewS3GatewayClient(conn)
 
+	filename := "/home/anon/Downloads/obama.jpg"
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
 	if _, err := client.PutImageObject(context.Background(), &s3file.ImageObject{
 		Id:       1,
-		Contents: []byte("hello world"),
+		Contents: dat,
 	}); err != nil {
 		panic(err)
 	}
