4494bed16649ebb8d683fd47a5f962f37906eaaa
Alexey Plenkin <aveplen@yandex.ru>
2022-06-17 15:28:47 +0300

    health check

--- cmd/s3/main.go
+++ cmd/s3/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/aveplen-bach/s3/internal/config"
 	"github.com/aveplen-bach/s3/internal/service"
@@ -20,16 +26,19 @@ import (
 )
 
 func main() {
+	// ============================== logger ==================================
 	zapLogger, err := zap.NewProduction()
 	if err != nil {
 		panic(err)
 	}
 
+	// ============================== config ==================================
 	config, err := config.ParseFromEnv()
 	if err != nil {
 		zapLogger.Fatal("failed to get config", zap.Error(err))
 	}
 
+	// =========================== minio client ===============================
 	minioClient, err := minio.New(
 		config.S3Addr,
 		&minio.Options{
@@ -45,6 +54,7 @@ func main() {
 		zapLogger.Fatal("failed to create minio client", zap.Error(err))
 	}
 
+	// =========================== minio service ==============================
 	minioService, err := service.NewMinioImplementation(minioClient, config.S3BucketName)
 	if err != nil {
 		zapLogger.Fatal("failed to initialize minio-service", zap.Error(err))
@@ -55,6 +65,7 @@ func main() {
 		zapLogger.Fatal("failed to listen to socket", zap.Error(err))
 	}
 
+	// ============================ grpc server ===============================
 	server := grpc.NewServer(
 		grpc.UnaryInterceptor(
 			grpc_middleware.ChainUnaryServer(
@@ -67,12 +78,39 @@ func main() {
 	grpcService := transport.New(minioService)
 	s3file.RegisterS3GatewayServer(server, grpcService)
 
-	go func() {
-		http.Handle("/metrics", promhttp.Handler())
-		http.ListenAndServe(config.HTTPListenAddr, http.DefaultServeMux)
-	}()
+	// ============================ health live ===============================
+	router := gin.Default()
+	router.GET("/s3g/health/live", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
 
+	// =============================== start ==================================
 	if err := server.Serve(grpcConn); err != nil {
 		zapLogger.Fatal("server failed", zap.Error(err))
 	}
+
+	srv := &http.Server{
+		Addr:    ":8082",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			zapLogger.Info("listening "+srv.Addr, zap.Error(err))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	zapLogger.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		zapLogger.Fatal("server forced to shutdown: ", zap.Error(err))
+	}
+
+	zapLogger.Info("server exiting")
 }
--- go.mod
+++ go.mod
@@ -15,10 +15,22 @@ require (
 )
 
 require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+)
+
+require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/gin-gonic/gin v1.8.1
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
